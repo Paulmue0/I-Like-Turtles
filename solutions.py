c7a2d7393256c45b1267d2257d97f12639e5282a
Paulmue0 <muellerpaul2000@live.de>
2023-02-04 04:51:10 +0100

    changed tasks

--- solutions.py
+++ solutions.py
@@ -8,23 +8,19 @@ def draw_square(turtle):
     for i in range(4):
         turtle.forward(100)
         turtle.right(90)
+
 def draw_triangle(turtle):
     for i in range(3):
         turtle.forward(100)
         turtle.left(120)
+
 def draw_circle(turtle):
     turtle.circle(50)
+    
 def draw_star(turtle):
     for i in range(5):
         turtle.forward(100)
         turtle.right(144)
-def draw_hexagon(turtle):
-    for i in range(6):
-        turtle.forward(100)
-        turtle.right(60)
 
-
-
-
-exercise_list = [draw_square, draw_triangle, draw_circle, draw_star, draw_hexagon]
+exercise_list = [draw_square, draw_triangle, draw_circle, draw_star]
 app = TurtleApp(exercise_list)
